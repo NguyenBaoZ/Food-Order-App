6f40becb2905e470c6c611497b2fa460eb6ffb72
Johnny-Luu <sangjohn58@gmail.com>
2021-05-15 14:30:23 +0700

    update form profile, add some asset, processing checkout form

--- app/src/main/java/com/example/orderfoodapp/LoginActivity.kt
+++ app/src/main/java/com/example/orderfoodapp/LoginActivity.kt
@@ -1,31 +1,22 @@
 package com.example.orderfoodapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
-import android.widget.*
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import kotlin.math.sign
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
-    private lateinit var email_editText: EditText
-    private lateinit var password_editText: EditText
-    private lateinit var login_button: Button
-    private lateinit var signup_textView: TextView
-
-    private lateinit var mAuth: FirebaseAuth
+   private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        email_editText = findViewById(R.id.email_editText)
-        password_editText = findViewById(R.id.password_editText)
-        login_button = findViewById(R.id.login_button)
-        signup_textView = findViewById(R.id.signup_textView)
 
         mAuth = Firebase.auth
 
--- app/src/main/java/com/example/orderfoodapp/MainMenuActivity.kt
+++ app/src/main/java/com/example/orderfoodapp/MainMenuActivity.kt
@@ -2,10 +2,12 @@ package com.example.orderfoodapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main_menu.*
 
-class Main_Menu_Activity : AppCompatActivity() {
+class MainMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+
     }
 }
