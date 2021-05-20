2d4628ea2487824ea26fe730ff64813340af40b4
GiaBao2001zz <tranhoanggiabao2001@gmail.com>
2021-05-20 11:18:21 +0700

    Add food-detail UI

---
+++ app/src/main/java/com/example/orderfoodapp/FoodDetail.kt
@@ -0,0 +1,11 @@
+package com.example.orderfoodapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FoodDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food_detail)
+    }
+}
